80dc1ed8fbe1241212260f070559af694d0a4d08
Andrey Ryabtsev <kodacci@gmail.com>
2025-02-11 18:00:22 +0300

    feat: #4 adds additional request errors logging

--- core/src/main/java/pro/ra_tech/giga_ai_agent/core/controllers/BaseController.java
+++ core/src/main/java/pro/ra_tech/giga_ai_agent/core/controllers/BaseController.java
@@ -1,6 +1,7 @@
 package pro.ra_tech.giga_ai_agent.core.controllers;
 
 import io.vavr.control.Either;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -11,6 +12,7 @@ import pro.ra_tech.giga_ai_agent.failure.AppFailure;
 import java.net.URI;
 import java.util.Optional;
 
+@Slf4j
 public abstract class BaseController {
     private ProblemDetail toProblemDetail(AppFailure failure) {
         val problem = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), failure.getDetail());
@@ -18,13 +20,17 @@ public abstract class BaseController {
         problem.setProperty("code", failure.getCode());
         problem.setProperty("source", failure.getSource());
         problem.setProperty("message", Optional.ofNullable(failure.getCause()).map(Throwable::getMessage).orElse("Unknown error"));
+        problem.setProperty("trace", Optional.ofNullable(failure.getCause()).map(Throwable::getStackTrace).orElse(null));
 
         return problem;
     }
 
     protected <T> ResponseEntity<Object> toResponse(Either<AppFailure, T> result) {
         return result.fold(
-                failure -> new ResponseEntity<Object>(toProblemDetail(failure), HttpStatus.INTERNAL_SERVER_ERROR),
+                failure -> {
+                    log.error("Request error:", failure.getCause());
+                    return new ResponseEntity<Object>(toProblemDetail(failure), HttpStatus.INTERNAL_SERVER_ERROR);
+                },
                 data -> new ResponseEntity<Object>(data, HttpStatus.OK)
         );
     }
