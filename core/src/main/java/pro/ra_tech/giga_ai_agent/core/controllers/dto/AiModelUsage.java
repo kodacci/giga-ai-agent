1832f02d2dc8b97af56fb5a417254e12819b7766
Andrey Ryabtsev <kodacci@gmail.com>
2025-02-12 16:18:00 +0300

    feat: #4 adds additional data to ask response

---
+++ core/src/main/java/pro/ra_tech/giga_ai_agent/core/controllers/dto/AiModelUsage.java
@@ -0,0 +1,17 @@
+package pro.ra_tech.giga_ai_agent.core.controllers.dto;
+
+public record AiModelUsage(
+        Integer promptTokens,
+        Integer completionTokens,
+        Integer precachedPromptTokens,
+        Integer totalTokens
+) {
+    public static AiModelUsage of(pro.ra_tech.giga_ai_agent.integration.rest.model.AiModelUsage usage) {
+        return new AiModelUsage(
+                usage.promptTokens(),
+                usage.completionTokens(),
+                usage.precachedPromptTokens(),
+                usage.totalTokens()
+        );
+    }
+}
--- core/src/main/java/pro/ra_tech/giga_ai_agent/core/controllers/dto/AskAiModelResponse.java
+++ core/src/main/java/pro/ra_tech/giga_ai_agent/core/controllers/dto/AskAiModelResponse.java
@@ -1,5 +1,6 @@
 package pro.ra_tech.giga_ai_agent.core.controllers.dto;
 
+import lombok.val;
 import pro.ra_tech.giga_ai_agent.integration.rest.model.AiModelAnswerResponse;
 import pro.ra_tech.giga_ai_agent.integration.rest.model.AiModelChoice;
 import pro.ra_tech.giga_ai_agent.integration.rest.model.AiModelChoiceMessage;
@@ -8,18 +9,18 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
-public record AskAiModelResponse(List<String> messages) {
+public record AskAiModelResponse(List<String> messages, AiModelUsage usage) {
     public static AskAiModelResponse of(AiModelAnswerResponse answer) {
-        return new AskAiModelResponse(
-                answer.choices().stream()
-                        .map(AiModelChoice::message)
-                        .map(
-                                message -> Optional.ofNullable(message)
-                                        .map(AiModelChoiceMessage::content)
-                                        .orElse(null)
-                        )
-                        .filter(Objects::nonNull)
-                        .toList()
-        );
+        val messages = answer.choices().stream()
+                .map(AiModelChoice::message)
+                .map(
+                        message -> Optional.ofNullable(message)
+                                .map(AiModelChoiceMessage::content)
+                                .orElse(null)
+                )
+                .filter(Objects::nonNull)
+                .toList();
+
+        return new AskAiModelResponse(messages, AiModelUsage.of(answer.usage()));
     }
 }
