64573b7692eb8a269011a38b743cd6fce37bbcba
Andrey Ryabtsev <kodacci@gmail.com>
2025-02-15 00:17:54 +0300

    fix: #5 fixes telegram api requests serialization

--- integration/src/main/java/pro/ra_tech/giga_ai_agent/integration/rest/telegram/model/SendMessageRequest.java
+++ integration/src/main/java/pro/ra_tech/giga_ai_agent/integration/rest/telegram/model/SendMessageRequest.java
@@ -1,8 +1,10 @@
 package pro.ra_tech.giga_ai_agent.integration.rest.telegram.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.lang.Nullable;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record SendMessageRequest(
         @JsonProperty("chat_id") long chatId,
         @JsonProperty("text") String text,
