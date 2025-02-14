86776de58c2153707072ac3a8ab4ecfd5f80d139
Andrey Ryabtsev <kodacci@gmail.com>
2025-02-15 00:05:05 +0300

    fix: #5 fixes user role name

--- integration/src/main/java/pro/ra_tech/giga_ai_agent/integration/rest/giga/model/AiRole.java
+++ integration/src/main/java/pro/ra_tech/giga_ai_agent/integration/rest/giga/model/AiRole.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum AiRole {
     SYSTEM("system"),
-    USER("from"),
+    USER("user"),
     ASSISTANT("assistant"),
     FUNCTION("function");
 
