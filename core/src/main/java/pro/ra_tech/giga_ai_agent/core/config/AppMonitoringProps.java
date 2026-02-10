11c9b9124c2410e4caf7a805c06059ede3dba4a4
Andrey <kodacci@gmail.com>
2026-02-10 22:48:00 +0300

    Feature/65 update deps (#66)
    * feat: #65 updates deps and test vars
    
    * fix: #65 fixes vars
    
    * feat: #65 updates java version
    
    * feat: #65 updates log4j version

--- core/src/main/java/pro/ra_tech/giga_ai_agent/core/config/AppMonitoringProps.java
+++ core/src/main/java/pro/ra_tech/giga_ai_agent/core/config/AppMonitoringProps.java
@@ -1,12 +1,18 @@
 package pro.ra_tech.giga_ai_agent.core.config;
 
+import jakarta.validation.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("app.monitoring")
 public record AppMonitoringProps (
+        @NotEmpty
         String appName,
+        @NotEmpty
         String appVersion,
+        @NotEmpty
         String podName,
+        @NotEmpty
         String podNamespace,
+        @NotEmpty
         String nodeName
 ) {}
